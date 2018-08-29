e6d5b321b7a957bc538c378203e516f5446fe24d
David <david@nustechnology.com>
2018-08-29 08:07:10 +0700

    Random sleep seconds

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from pynput import mouse, keyboard
 import time
 from helper import Helper
+from random import randint
 
 def run():
-  print('RUN')
+  time.sleep(randint(5, 10))
   helper = Helper(keyboard.Controller(), keyboard.Key)
   helper.alt_tab_step(10)
 
-for i in range(1,4):
+for i in range(1, 4):
   print(i)
   time.sleep(1)
 
 print('GO')
 while(True):
-  time.sleep(10)
   run()
