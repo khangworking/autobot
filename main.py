865c9f89cec2a8a2f71cb0a4ff77c15da32b467e
David <david@nustechnology.com>
2018-11-21 16:57:09 +0700

    poweroff

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pynput import mouse, keyboard
 import time
+import datetime
 from helper import Helper
 from random import randint
+import os
 
 def run():
   time.sleep(randint(30, 50))
@@ -10,6 +12,8 @@ def run():
   helper.ctrl_pgDw_step(randint(5, 10))
   for i in range(randint(10, 20)):
     mouse.Controller().move(randint(10, 100), randint(10, 100))
+  if datetime.datetime.now().hour >= 17
+    os.system("poweroff")
 
 for i in range(1, 4):
   print(i)
